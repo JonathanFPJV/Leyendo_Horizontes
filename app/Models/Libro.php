4777e5bf24941c5a7e7674e973ef58681ec87fee
JonathanFPJV <168221566+JonathanFPJV@users.noreply.github.com>
2024-06-12 18:12:21 -0500

    migraciones completas

---
+++ app/Models/Libro.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Libro extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'anio',
+        'autor',
+        'editorial',
+        'stock',
+        'disponibilidad',
+    ];
+
+    public function prestamos()
+    {
+        return $this->hasMany(Prestamo::class);
+    }
+}
---
+++ app/Models/Prestamo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prestamo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'libro_id',
+        'fecha_inicio',
+        'fecha_devolucion',
+        'devuelto',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function libro()
+    {
+        return $this->belongsTo(Libro::class);
+    }
+}
